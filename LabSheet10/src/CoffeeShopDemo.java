4c6218e0029f2b563d47ea4c96529ba94554f6c9
NaphachanokJT <junto1673@gmail.com>
2025-02-26 11:19:48 +0700

    LabSheet10/Homework

--- LabSheet10/src/CoffeeShopDemo.java
+++ LabSheet10/src/CoffeeShopDemo.java
@@ -1,9 +1,35 @@
+import javax.swing.*;
 
 public class CoffeeShopDemo {
+	
+   public static void main(String[] args) {
+	   
+	    Barista staff1 = new Barista("Leon", 'M');
+		Barista staff2 = new Barista("Claire", 'F');
+		
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int type = Integer.parseInt(JOptionPane.showInputDialog("Input drink type : "));
+		int typeCoffee = Integer.parseInt(JOptionPane.showInputDialog("Input coffee type : "));
+		char size = JOptionPane.showInputDialog("Enter coffee size :").charAt(0);
+		int baristaNum = Integer.parseInt(JOptionPane.showInputDialog("Enter barista number : "));
+		int baristaNum = Integer.parseInt(JOptionPane.showInputDialog("Input barista number : "));
 
+		Barista selectedBarista;
+		if (baristaNum == 2) {
+		selectedBarista = staff2;
+			selectedBarista = staff2;
+		} else {
+		selectedBarista = staff1;
+			selectedBarista = staff1;
+		}
+	
+		Coffee order = new Coffee(type,size, typeCoffee, selectedBarista);
+		JOptionPane.showMessageDialog(null, order.toString() + "\nBarista: " + selectedBarista.getName() + " (" + selectedBarista.getGenderName() + ")");
+	
+
+		Coffee order = new Coffee(type, typeCoffee, size);
+
+		JOptionPane.showMessageDialog(null, order.toString() + "\nBarista: " + selectedBarista.getName() + " (" + selectedBarista.getGenderName() + ")");
 	}
 
 }
